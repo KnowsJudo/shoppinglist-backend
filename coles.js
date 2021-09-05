11eb89301abe3095b215ca9ae54cb4f5ce146c46
Lachlan Bardwell <75568181+KnowsJudo@users.noreply.github.com>
2021-09-05 22:12:40 +1000

    initial commit

---
+++ coles.js
@@ -0,0 +1,102 @@
+exports.Coles = {
+  produce: [
+    {
+      id: "Apples",
+      weight: 1000,
+      color: "Pink",
+      price: 3.99,
+    },
+    {
+      id: "Strawberries",
+      weight: 250,
+      color: "Red",
+      price: 2.99,
+    },
+    {
+      id: "Tomatoes",
+      weight: 500,
+      color: "red",
+      price: 8.99,
+    },
+    {
+      id: "Blueberries",
+      weight: 200,
+      color: "Blue",
+      price: 2.99,
+    },
+    {
+      id: "Cashews",
+      weight: 400,
+      color: "Beige",
+      price: 11.99,
+    },
+    {
+      id: "Onions",
+      weight: 100,
+      color: "Brown",
+      price: 0.8,
+    },
+    {
+      id: "Carrots",
+      weight: 1000,
+      color: "Orange",
+      price: 2.99,
+    },
+    {
+      id: "Broccolini",
+      weight: 200,
+      color: "Green",
+      price: 2.99,
+    },
+    {
+      id: "Avocados",
+      weight: 200,
+      color: "Green",
+      price: 1.5,
+    },
+    {
+      id: "Oranges",
+      weight: 3000,
+      color: "Orange",
+      price: 6.99,
+    },
+    {
+      id: "Potatoes",
+      weight: 200,
+      color: "White",
+      price: 1.2,
+    },
+    {
+      id: "Bananas",
+      weight: 500,
+      color: "Yellow",
+      price: 3,
+    },
+  ],
+  deli: [
+    {
+      id: "Microwave meal",
+      weight: 300,
+      color: "White",
+      price: 7.5,
+    },
+    {
+      id: "Chicken Thigh kg",
+      weight: 400,
+      color: "White",
+      price: 6,
+    },
+    {
+      id: "Chicken Breast kg",
+      weight: 400,
+      color: "White",
+      price: 4,
+    },
+    {
+      id: "Sundried Tomatoes",
+      weight: 300,
+      color: "Red",
+      price: 6,
+    },
+  ],
+};
